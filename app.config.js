844434ff834393011c6b6a0ea88b239d3205e26d
Vandetho <thovandeth@gmail.com>
2022-10-17 19:51:17 +0700

    Adding new eas id

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-    if (process.env.APP_ENV === 'release') {
+    if (['release', 'production'].includes(process.env.APP_ENV)) {
         return require('./app.release.json');
     }
     return require('./app.beta.json');
